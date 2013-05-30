57e859f7f63cf2610cb12373be1be617085e31c1
Barry Wark <barrywark@gmail.com>
2013-05-30 15:14:58 -0400

    Upgrade to release platofom 7.3 and add login dialog

--- ovation-ui-database-connection-provider/src/main/java/us/physion/ovation/ui/database/DatabaseConnectionProvider.java
+++ ovation-ui-database-connection-provider/src/main/java/us/physion/ovation/ui/database/DatabaseConnectionProvider.java
@@ -46,6 +46,7 @@ import us.physion.ovation.ui.interfaces.ConnectionProvider;
 public class DatabaseConnectionProvider implements ConnectionProvider{
 
     static Logger logger = LoggerFactory.getLogger(DatabaseConnectionProvider.class);
+    
     private JTextField addField(JPanel form, String name, int row, boolean passwordField) {
         JTextField f;
         if (passwordField)
@@ -68,18 +69,18 @@ public class DatabaseConnectionProvider implements ConnectionProvider{
 
     private static class LoginModel {
         String email; 
-        String password; 
+        char[] password; 
         boolean cancelled = true;
 
         void setEmail(String email)
         {
             this.email = email;
         }
-        void setPassword(String pw)
+        void setPassword(char[] pw)
         {
             this.password = pw;
         }
-        String getPassword()
+        char[] getPassword()
         {
             return password;
         }
@@ -99,6 +100,7 @@ public class DatabaseConnectionProvider implements ConnectionProvider{
     public DatabaseConnectionProvider() {
         
         connectionListeners = Collections.synchronizedSet(new HashSet());
+        Logging.configureRootLoggerRollingAppender();
     }
 
     public synchronized void resetConnection()
@@ -152,123 +154,29 @@ public class DatabaseConnectionProvider implements ConnectionProvider{
         
         final LoginModel model = new LoginModel();
         
-        final JDialog d = new JDialog(new JFrame(), true);
+        final LoginDialog d = new LoginDialog(new JFrame(), true);
         d.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+        d.addLoginActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent ae) {
+                model.setEmail(d.getEmail());
+                model.setPassword(d.getPassword());
+                model.cancelled = false;
+                d.getBusyLabel().setBusy(true);
+                d.dispose();
+            }
+        });
         
-        //JTabbedPane tabs = new JTabbedPane(JTabbedPane.BOTTOM);
-        JPanel login = new JPanel();
-        login.setLayout(new BoxLayout(login, BoxLayout.PAGE_AXIS));
-        //tabs.addTab("Login", login);
-        
-        //JPanel signUp = new JPanel();
-        //tabs.addTab("Sign up", signUp);
-        
-        //LOGIN
-        //------------------------------------------------------
-        //TODO: header if the error is not null
-        
-        //two text fields
-        JPanel form = new JPanel(new GridBagLayout());
-        final JTextField emailTB = addField(form, "Email: ", 0, false);
-        final JTextField passwordTB = addField(form, "Password: ", 1, true);
-        
-        //Cancel/Ok buttons
-        JPanel buttonPane = new JPanel();
-        //JButton cancelButton = new JButton("Cancel");
-        JButton okButton = new JButton("Login");
-        buttonPane.setLayout(new BoxLayout(buttonPane, BoxLayout.PAGE_AXIS));
-        buttonPane.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
-        buttonPane.add(okButton);
-
-        buttonPane.add(Box.createHorizontalGlue());
-        JCheckBox cb = new JCheckBox();
-        cb.setSelected(true);
-        buttonPane.add(cb);
-        buttonPane.add(Box.createRigidArea(new Dimension(10, 0)));
-        JLabel label = new JLabel("Work offline");
-        buttonPane.add(label);
-        BufferedImage physionIcon;
-        File f = null;
-        try {
-            f = new File ("installer/ovation_48x48.png");
-            //f = new File("../branding/src/main/nbm-branding/core/core.jar/org/netbeans/core/startup/splash.gif");
-            physionIcon = ImageIO.read(f);
-            JLabel image = new JLabel(new ImageIcon( physionIcon ));
-            image.setPreferredSize(buttonPane.getPreferredSize());
-            buttonPane.add(image);
-        }
-        catch (IOException ex) {
-            String s = "";
-            if (f != null)
-                s = " at '" + f.getAbsolutePath() + "'";
-            logger.error("Could not find Physion icon" + s);
-        }
-        /*cancelButton.addActionListener(new ActionListener() {
-
+        d.addCancelActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent ae) {
                 model.cancelled = true;
                 d.dispose();
             }
         });
-        *
-        */
-        okButton.addActionListener(new ActionListener() {
-
-            @Override
-            public void actionPerformed(ActionEvent ae) {
-                model.setEmail(emailTB.getText());
-                model.setPassword(passwordTB.getText());
-                model.cancelled = false;
-                d.dispose();
-            }
-        });
-               
-        login.add(form, BorderLayout.CENTER);
-        login.add(buttonPane, BorderLayout.PAGE_END);
         
-        //SIGN UP
-        //-----------------------------------------------------------
-       /* JLabel header = new JLabel("New to Ovation? Sign up");
-        
-        //two text fields
-        JPanel s_form = new JPanel(new GridBagLayout());
-        final JTextField nameTB = addField(s_form, "Name: ", 0, false);
-        final JTextField s_emailTB = addField(s_form, "Email: ", 1, false);
-        final JTextField s_passwordTB = addField(s_form, "Password: ", 2, true);
-        
-        JPanel s_buttonPane = new JPanel();
-        JButton signUpButton = new JButton("Sign Up");
-        s_buttonPane.setLayout(new BoxLayout(s_buttonPane, BoxLayout.LINE_AXIS));
-        s_buttonPane.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
-        s_buttonPane.add(signUpButton);
-        
-        signUpButton.addActionListener(new ActionListener() {
-
-            @Override
-            public void actionPerformed(ActionEvent ae) {
-                //sign up through the website
-                //when that's completed, 
-                model.setEmail(s_emailTB.getText());
-                model.setPassword(s_passwordTB.getText());
-                model.cancelled = false;
-                d.dispose();
-            }
-        });
-        signUp.add(header, BorderLayout.PAGE_START);
-        signUp.add(s_form, BorderLayout.CENTER);
-        signUp.add(s_buttonPane, BorderLayout.PAGE_END);
-        
-        d.getContentPane().add(tabs);
-        login.getRootPane().setDefaultButton(okButton);
-        */
-        d.getContentPane().setBackground(Color.WHITE);
-        d.getContentPane().add(login);
-        login.getRootPane().setDefaultButton(okButton);
         //show dialog
-        d.pack();
-        //Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
-        d.setLocationRelativeTo(null);
+        //d.setLocationRelativeTo(null);
         d.setVisible(true);
         return model;
     }
@@ -276,7 +184,7 @@ public class DatabaseConnectionProvider implements ConnectionProvider{
         LoginModel m = showLoginDialog(error);
         if (!m.isCancelled()) {
             try {
-                return dsc.authenticateUser(m.getEmail(), m.getPassword().toCharArray()).get();
+                return dsc.authenticateUser(m.getEmail(), m.getPassword()).get();
             } catch (AuthenticationException e) {
                 return authenticateUser(dsc, e.getLocalizedMessage());
             } catch (InterruptedException e) {
