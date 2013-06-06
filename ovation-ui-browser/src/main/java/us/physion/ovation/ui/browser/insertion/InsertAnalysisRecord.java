58b6c0ed59c1a77294667f2756444416c2bb0711
Barry Wark <barrywark@gmail.com>
2013-06-06 13:51:24 -0400

    Merged changes from ovation2/dev and switchted to ovation-logging

--- ovation-ui-browser/src/main/java/us/physion/ovation/ui/browser/insertion/InsertAnalysisRecord.java
+++ ovation-ui-browser/src/main/java/us/physion/ovation/ui/browser/insertion/InsertAnalysisRecord.java
@@ -4,23 +4,17 @@
  */
 package us.physion.ovation.ui.browser.insertion;
 
-import java.awt.event.ActionEvent;
-import java.text.MessageFormat;
-import java.util.*;
-import javax.swing.AbstractAction;
-import org.openide.DialogDisplayer;
 import org.openide.WizardDescriptor;
-import org.openide.util.NbBundle;
-import org.openide.util.Utilities;
-import org.openide.util.lookup.ServiceProvider;
 import us.physion.ovation.DataStoreCoordinator;
-import us.physion.ovation.domain.AnalysisRecord;
-import us.physion.ovation.domain.Epoch;
 import us.physion.ovation.domain.Project;
 import us.physion.ovation.domain.Protocol;
 import us.physion.ovation.domain.mixin.DataElement;
 import us.physion.ovation.ui.interfaces.IEntityWrapper;
 import us.physion.ovation.ui.interfaces.ProjectInsertable;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 //@ServiceProvider(service=ProjectInsertable.class)
 /**
@@ -39,7 +33,7 @@ public class InsertAnalysisRecord extends InsertEntity implements ProjectInserta
         List<WizardDescriptor.Panel<WizardDescriptor>> panels = new ArrayList<WizardDescriptor.Panel<WizardDescriptor>>();
         panels.add(new InsertAnalysisRecordWizardPanel1());
         panels.add(new InsertAnalysisRecordWizardPanel2());
-        panels.add(new InsertAnalysisRecordWizardPanel3()); 
+        panels.add(new InsertAnalysisRecordWizardPanel3());
         return panels;
     }
 
@@ -49,14 +43,12 @@ public class InsertAnalysisRecord extends InsertEntity implements ProjectInserta
         Protocol protocol = (Protocol)wiz.getProperty("analysisRecord.protocol");
         if (protocol == null)
             protocol = dsc.getContext().insertProtocol((String)wiz.getProperty("protocol.name"), (String)wiz.getProperty("protocol.name"));
-        ((Project)parent.getEntity()).insertAnalysisRecord(((String)wiz.getProperty("analysisRecord.name")), 
-                ((Set<DataElement>)wiz.getProperty("analysisRecord.unnamedInputs")), 
-                ((Map<String, DataElement>)wiz.getProperty("analysisRecord.namedInputs")), 
-                ((Set<DataElement>)wiz.getProperty("analysisRecord.entryFn")), 
-                protocol, 
+        ((Project)parent.getEntity()).insertAnalysisRecord(((String)wiz.getProperty("analysisRecord.name")),
+                ((Map<String, DataElement>)wiz.getProperty("analysisRecord.namedInputs")),
+                protocol,
                 ((Map<String, Object>)wiz.getProperty("analysisRecord.scmURL")));
     }
-    
+
     @Override
     public int getPosition() {
         return 300;
--- ovation-ui-data-viewer/src/main/java/us/physion/ovation/ui/editor/ChartVisualizationFactory.java
+++ ovation-ui-data-viewer/src/main/java/us/physion/ovation/ui/editor/ChartVisualizationFactory.java
@@ -4,15 +4,14 @@
  */
 package us.physion.ovation.ui.editor;
 
-import java.util.concurrent.ExecutionException;
 import org.jfree.data.xy.DefaultXYDataset;
-import org.openide.util.Exceptions;
 import org.openide.util.lookup.ServiceProvider;
 import us.physion.ovation.domain.Measurement;
-import us.physion.ovation.domain.NumericMeasurementUtils;
+import us.physion.ovation.domain.NumericDataElements;
 import us.physion.ovation.exceptions.OvationException;
 import us.physion.ovation.values.NumericData;
-import us.physion.ovation.values.NumericData.Data;
+
+import java.util.concurrent.ExecutionException;
 
 @ServiceProvider(service = VisualizationFactory.class)
 /**
@@ -23,23 +22,23 @@ public class ChartVisualizationFactory implements VisualizationFactory{
 
    @Override
     public Visualization createVisualization(Measurement r) {
-       
-        if (!NumericMeasurementUtils.isNumericMeasurement(r))
+
+        if (!NumericDataElements.isNumeric(r))
         {
             throw new OvationException("Can only plot Numeric data with the ChartVisualization plugin");
         }
         NumericData data;
             try {
-                data = NumericMeasurementUtils.getNumericData(r).get();
+                data = NumericDataElements.getNumericData(r).get();
             } catch (InterruptedException ex) {
                 throw new OvationException(ex.getMessage());
             } catch (ExecutionException ex) {
                 throw new OvationException(ex.getMessage());
             }
-        
+
         ChartGroupWrapper g = new ChartGroupWrapper(new DefaultXYDataset(), data);
         g.setTitle(r.getName());
-        
+
         for (NumericData.Data d : data.getData().values())
         {
             g.addXYDataset(d);
@@ -49,10 +48,10 @@ public class ChartVisualizationFactory implements VisualizationFactory{
 
     @Override
     public int getPreferenceForDataContainer(Measurement r) {
-        if (NumericMeasurementUtils.isNumericMeasurement(r))
+        if (NumericDataElements.isNumeric(r))
         {
             try {
-                if (NumericMeasurementUtils.getNumericData(r).get().getData().size() == 1);
+                if (NumericDataElements.getNumericData(r).get().getData().size() == 1);
             } catch (InterruptedException ex) {
                 return -1;
             } catch (ExecutionException ex) {
