80a2700837686e9bb63d0cf2f61823df9ba3670d
Jackie <jackie@physionconsulting.com>
2013-10-29 16:00:02 -0400

    Cherry-picking part of EventQueueUtilities from another branch.

--- common-interfaces/src/main/java/us/physion/ovation/ui/interfaces/EventQueueUtilities.java
+++ common-interfaces/src/main/java/us/physion/ovation/ui/interfaces/EventQueueUtilities.java
@@ -1,15 +1,21 @@
 package us.physion.ovation.ui.interfaces;
+import com.google.common.util.concurrent.FutureCallback;
+import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.ListenableFuture;
+import com.google.common.util.concurrent.ListeningExecutorService;
+import com.google.common.util.concurrent.MoreExecutors;
 import java.awt.EventQueue;
 import javax.swing.SwingUtilities;
 import java.lang.reflect.InvocationTargetException;
-import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.FutureTask;
 import java.util.concurrent.Future;
+import org.netbeans.api.progress.ProgressHandle;
 
 public class EventQueueUtilities
 {
-    private static ExecutorService executorService = Executors.newFixedThreadPool(5);
+    private static ListeningExecutorService executorService = MoreExecutors.listeningDecorator(
+            Executors.newCachedThreadPool());
 
     public static void runOnEDT(Runnable r) {
 	if (EventQueue.isDispatchThread()) {
@@ -40,5 +46,52 @@ public class EventQueueUtilities
 	    t.run();
 	    return t;
 	}
+    }
+    
+    private static void start(final ProgressHandle ph)
+    {
+        runOnEDT(new Runnable(){
+
+                        @Override
+                        public void run() {
+                            ph.start();                        
+                        }
+                    });
+    }
+    
+    private static void finish(final ProgressHandle ph)
+    {
+        runOnEDT(new Runnable(){
+
+                        @Override
+                        public void run() {
+                            ph.finish();                        
+                        }
+                    });
+    }
+    
+    
+    public static Future runOffEDT(Runnable r, final ProgressHandle ph) {
+        if (EventQueue.isDispatchThread()) {
+            start(ph);
+            ListenableFuture f = executorService.submit(r);
+            Futures.addCallback(f, new FutureCallback() {
+                @Override
+                public void onSuccess(Object result) {
+                    finish(ph);
+                }
+
+                @Override
+                public void onFailure(Throwable t) {
+                    finish(ph);
+                }
+            });
+            return f;
+        } else {
+            start(ph);
+            r.run();
+            finish(ph);
+            return Futures.immediateFuture(null);
+        }
     }
 }
