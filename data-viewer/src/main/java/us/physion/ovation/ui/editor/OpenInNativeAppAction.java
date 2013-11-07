24c941fc51e72b1b2b6b59271f62e3d68a06bdcc
Emilian Bold <emilian.bold@josekibold.ro>
2013-11-07 20:00:55 +0200

    Calling getData asynchronously.

--- data-viewer/src/main/java/us/physion/ovation/ui/editor/OpenInNativeAppAction.java
+++ data-viewer/src/main/java/us/physion/ovation/ui/editor/OpenInNativeAppAction.java
@@ -5,34 +5,66 @@ import java.awt.event.ActionEvent;
 import java.io.File;
 import java.io.IOException;
 import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Future;
 import javax.swing.AbstractAction;
+import org.netbeans.api.progress.ProgressHandleFactory;
+import org.openide.util.NbBundle.Messages;
+import us.physion.ovation.domain.mixin.DataElement;
 import us.physion.ovation.exceptions.OvationException;
+import us.physion.ovation.ui.interfaces.EventQueueUtilities;
 
+@Messages({
+    "Open_in_native_application_button=Open in native application...",
+    "# {0} - data element name",
+    "Getting_and_opening_file=Getting and opening file for {0}",
+    "Java_Desktop_not_supported=Java Desktop not supported on this machine"
+})
 public class OpenInNativeAppAction extends AbstractAction {
 
-    private final Future<File> fileFuture;
+    private final DataElement fileFuture;
     private final File file;
 
     public OpenInNativeAppAction(File file) {
         this(file, null);
     }
-    
-    public OpenInNativeAppAction(Future<File> fileFuture) {
+
+    public OpenInNativeAppAction(DataElement fileFuture) {
         this(null, fileFuture);
     }
-    
-    private OpenInNativeAppAction(File file, Future<File> fileFuture) {
-        //TODO: L10N
-        super("Open in native application...");
+
+    private OpenInNativeAppAction(File file, DataElement fileFuture) {
+        super(Bundle.Open_in_native_application_button());
         this.file = file;
         this.fileFuture = fileFuture;
     }
 
     @Override
     public void actionPerformed(ActionEvent ae) {
+        if (fileFuture == null) {
+            openFile(file);
+        } else {
+            EventQueueUtilities.runOffEDT(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        final File f = fileFuture.getData().get();
+                        EventQueueUtilities.runOnEDT(new Runnable() {
+                            @Override
+                            public void run() {
+                                openFile(f);
+                            }
+                        });
+                    } catch (InterruptedException ex) {
+                        throw new OvationException(ex);
+                    } catch (ExecutionException ex) {
+                        throw new OvationException(ex);
+                    }
+                }
+            }, ProgressHandleFactory.createHandle(Bundle.Getting_and_opening_file(fileFuture.getName())));
+        }
+    }
+
+    private void openFile(File f) {
         try {
-            File f = fileFuture != null ? fileFuture.get() : file;
             if (Desktop.isDesktopSupported()) {
                 if (f.getAbsolutePath().length() > 254) {
                     Desktop.getDesktop().open(f.getParentFile());
@@ -40,13 +72,8 @@ public class OpenInNativeAppAction extends AbstractAction {
                     Desktop.getDesktop().open(f);
                 }
             } else {
-                //TODO: L10N
-                throw new OvationException("Java Desktop not supported on this machine");
+                throw new OvationException(Bundle.Java_Desktop_not_supported());
             }
-        } catch (InterruptedException ex) {
-            throw new OvationException(ex);
-        } catch (ExecutionException ex) {
-            throw new OvationException(ex);
         } catch (IOException ex) {
             throw new OvationException(ex);
         }
