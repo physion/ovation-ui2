6475c35184f86aaec91a6f42382463d50aab29cf
Jackie <jackie@physionconsulting.com>
2014-02-07 12:14:59 -0500

    search dialog

--- browser/src/main/java/us/physion/ovation/ui/browser/SearchAction.java
+++ browser/src/main/java/us/physion/ovation/ui/browser/SearchAction.java
@@ -4,13 +4,20 @@
  */
 package us.physion.ovation.ui.browser;
 
+import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.concurrent.Future;
+import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JTextField;
 import org.openide.awt.ActionID;
 import org.openide.awt.ActionReference;
+import org.openide.awt.ActionReferences;
 import org.openide.awt.ActionRegistration;
 import org.openide.util.Lookup;
 import org.openide.util.NbBundle.Messages;
@@ -24,34 +31,59 @@ import us.physion.ovation.ui.interfaces.EventQueueUtilities;
         category = "Edit",
         id = "us.physion.ovation.ui.browser.SearchAction")
 @ActionRegistration(
+        //iconBase = "us/physion/ovation/ui/browser/reset-query.png",
         displayName = "#CTL_SearchAction")
-@ActionReference(path = "Menu/File", position = 1300)
+@ActionReferences({
+    @ActionReference(path = "Menu/Tools", position = 1100),
+    @ActionReference(path = "Shortcuts", name = "DS-S")
+})
 @Messages("CTL_SearchAction=Search")
 public final class SearchAction implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        /*JFrame f = new JFrame();
-        f.add(new JLabel("Search by a tag"));
-        JComboBox box = new JComboBox();
-        box.setEditable(true);
-        f.add(box);
-        f.pack();
-        f.setVisible(true);*/
-        
-        EventQueueUtilities.runOffEDT(new Runnable() {
+        final JFrame f = new JFrame();
+        f.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        f.setLayout(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
+        c.gridx = 0;
+        c.gridy = 0;
+        final JTextField text = new JTextField();
+        text.setEditable(true);
+        text.setPreferredSize(new Dimension(200, 20));
+        f.add(text, c);
+        JButton submit = new JButton("Search");
+        submit.setEnabled(true);
+        c.gridx = 1;
+        f.add(submit, c);
+        submit.addActionListener(new ActionListener() {
+
             @Override
-            public void run() {
-                DataContext context = Lookup.getDefault().lookup(ConnectionProvider.class).getNewContext();
-                QuerySet querySet = new QuerySet();
-                Lookup.getDefault().lookup(QueryProvider.class).setQuerySet(querySet);
-                for (Taggable entity: context.getObjectsWithTag("tag"))
-                {
-                    querySet.add((OvationEntity)entity);
-                }
+            public void actionPerformed(ActionEvent e) {
+                Future future = EventQueueUtilities.runOffEDT(new Runnable() {
+                    @Override
+                    public void run() {
+                        DataContext context = Lookup.getDefault().lookup(ConnectionProvider.class).getNewContext();
+                        QuerySet querySet = new QuerySet();
+                        Lookup.getDefault().lookup(QueryProvider.class).setQuerySet(querySet);
+                        for (String tag : getTags(text.getText())) {
+                            for (Taggable entity : context.getObjectsWithTag(tag)) {
+                                querySet.add((OvationEntity) entity);
+                            }
+                        }
+                        f.dispose();
+                    }
+                });
             }
         });
+        
+        f.pack();
+        f.setLocationRelativeTo(null);
+        f.setVisible(true);
+        
     }
-    
-    
+
+    private String[] getTags(String text) {
+        return text.replaceAll("\\s+","").split(",");
+    }
 }
