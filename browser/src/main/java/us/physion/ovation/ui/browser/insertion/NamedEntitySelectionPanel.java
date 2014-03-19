5726659121bca1f30dbb39235e3dbe3c59551f1a
Barry Wark <barrywark@gmail.com>
2014-03-19 18:02:32 -0400

    Multiple insertion and removal checked

--- browser/src/main/java/us/physion/ovation/ui/browser/insertion/NamedEntitySelectionPanel.java
+++ browser/src/main/java/us/physion/ovation/ui/browser/insertion/NamedEntitySelectionPanel.java
@@ -5,11 +5,14 @@
 package us.physion.ovation.ui.browser.insertion;
 
 import com.google.common.base.Function;
+import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
 import java.awt.Point;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import javax.swing.BoxLayout;
@@ -19,6 +22,9 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JSplitPane;
 import javax.swing.JTable;
+import javax.swing.ListSelectionModel;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 import org.openide.explorer.ExplorerManager;
 import org.openide.explorer.ExplorerUtils;
 import org.openide.explorer.view.BeanTreeView;
@@ -36,9 +42,9 @@ import us.physion.ovation.ui.interfaces.ParameterTableModel;
  *
  * @author huecotanks
  */
-public abstract class NamedEntitySelectionPanel extends JPanel implements 
+public abstract class NamedEntitySelectionPanel extends JPanel implements
         ExplorerManager.Provider, Lookup.Provider {
-    
+
     private JLabel descriptionLabel;
     final ChangeSupport cs;
     public DataContext context;
@@ -49,13 +55,13 @@ public abstract class NamedEntitySelectionPanel extends JPanel implements
     private JButton removeButton;
     private String labelText;
     private final Set<IEntityWrapper> selectedEntities;
-    
+    private final Set<Integer> selectedTableRows;
+
     private ParameterTableModel tableModel;
     private Lookup entitiesLookup;
     private ExplorerManager explorerManager;
-    
-    NamedEntitySelectionPanel(ChangeSupport cs, String labelText)
-    {
+
+    NamedEntitySelectionPanel(ChangeSupport cs, String labelText) {
         super();
         this.cs = cs;
         this.labelText = labelText;
@@ -69,20 +75,20 @@ public abstract class NamedEntitySelectionPanel extends JPanel implements
                 return input.x == 0; //only the first column is editable
             }
         });
-        
+
         selectedEntities = Sets.newHashSet();
-        
+        selectedTableRows = Sets.newHashSet();
+
         initComponents();
     }
 
     private void initComponents() {
         setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
-        if (labelText != null)
-        {
+        if (labelText != null) {
             descriptionLabel = new JLabel(labelText);
             this.add(descriptionLabel);
         }
-        
+
         jSplitPane1 = new JSplitPane();
         entityScrollPane = new JScrollPane();
         jSplitPane1.setLeftComponent(entityScrollPane);
@@ -91,13 +97,12 @@ public abstract class NamedEntitySelectionPanel extends JPanel implements
         namedEntitiesTable.setEnabled(true);
         jSplitPane1.setRightComponent(namedEntitiesTable);
         jSplitPane1.setDividerLocation(300);
-        
+
         explorerManager = new ExplorerManager();
         entitiesLookup = ExplorerUtils.createLookup(explorerManager, getActionMap());
         BeanTreeView entitiesTree = new BeanTreeView();
         entitiesTree.setRootVisible(false);
         entityScrollPane.setViewportView(entitiesTree);
-        
 
         addButton = new JButton("+");
         removeButton = new JButton("-");
@@ -112,7 +117,7 @@ public abstract class NamedEntitySelectionPanel extends JPanel implements
                 addSelection();
             }
         });
-        
+
         removeButton.addActionListener(new ActionListener() {
 
             @Override
@@ -124,8 +129,9 @@ public abstract class NamedEntitySelectionPanel extends JPanel implements
         buttonPanel.setAlignmentX(buttonPanel.LEFT_ALIGNMENT);
         this.add(jSplitPane1);
         this.add(buttonPanel);
-        
+
         addButton.setEnabled(false);
+        removeButton.setEnabled(false);
 
         final Lookup.Result<IEntityWrapper> selectionResult
                 = entitiesLookup.lookupResult(IEntityWrapper.class);
@@ -139,79 +145,106 @@ public abstract class NamedEntitySelectionPanel extends JPanel implements
                     addButton.setEnabled(true);
                     selectedEntities.addAll(selection);
                 }
-                
+
                 addButton.setEnabled(selectedEntities.size() > 0);
             }
         });
+        
+        namedEntitiesTable.getSelectionModel().addListSelectionListener(
+                new ListSelectionListener() {
+
+                    @Override
+                    public void valueChanged(ListSelectionEvent e) {
+                        if(e.getValueIsAdjusting()) {
+                            return;
+                        }
+                        
+                        ListSelectionModel lsm = (ListSelectionModel) e.getSource();
+
+                        selectedTableRows.clear();
+                        if (!lsm.isSelectionEmpty()) {
+
+                            // Find out which indexes are selected.
+                            int minIndex = lsm.getMinSelectionIndex();
+                            int maxIndex = lsm.getMaxSelectionIndex();
+                            for (int i = minIndex; i <= maxIndex; i++) {
+                                if (lsm.isSelectedIndex(i)) {
+                                    selectedTableRows.add(i);
+                                }
+                            }
+                        }
+                        
+                        removeButton.setEnabled(selectedTableRows.size() > 0);
+
+                    }
+                });
 
         resetEntities();
     }
 
     @Override
-    public String getName()
-    {
+    public String getName() {
         return getTitle();
     }
-    
+
     abstract String getTitle();
-    
-    public void addSelection()
-    {
-        for(IEntityWrapper e : getSelectedEntities()) {
+
+    public void addSelection() {
+        for (IEntityWrapper e : getSelectedEntities()) {
             addEntity(e.getDisplayName(), e.getEntity());
         }
     }
-    
+
     protected Set<IEntityWrapper> getSelectedEntities() {
         return selectedEntities;
     }
-    
-    public void removeSelection()
-    {
-        int num = namedEntitiesTable.getSelectedRow();
-        tableModel.remove(num);
-        NamedEntitySelectionPanel.this.cs.fireChange();
+
+    protected Set<Integer> getSelectedTableRows() {
+        return selectedTableRows;
     }
-    
-    public void addEntity(String name, OvationEntity value)
-    {   
-        int keysCount = tableModel.countKeys(name);
-        if (keysCount > 0)
-        {
-            tableModel.addParameter(name + "." + (keysCount+1), value);
+
+    public void removeSelection() {
+        List<Integer> selection = Lists.newArrayList(getSelectedTableRows());
+        Collections.sort(selection, Collections.reverseOrder());
+        for (int i : selection) {
+            tableModel.remove(i);
+            NamedEntitySelectionPanel.this.cs.fireChange();
         }
-        else{
+    }
+
+    public void addEntity(String name, OvationEntity value) {
+        int keysCount = tableModel.countKeys(name);
+        if (keysCount > 0) {
+            tableModel.addParameter(name + "." + (keysCount + 1), value);
+        } else {
             tableModel.addParameter(name, value);
         }
         NamedEntitySelectionPanel.this.cs.fireChange();
     }
-    
-    public Map<String, Object> getNamedEntities()
-    {
+
+    public Map<String, Object> getNamedEntities() {
         return tableModel.getParams();
     }
-    
+
     abstract public void resetEntities();
-    
-    public void addSelectedEntities(Map<String, OvationEntity> entities)
-    {
-        if (entities == null)
+
+    public void addSelectedEntities(Map<String, OvationEntity> entities) {
+        if (entities == null) {
             return;
-        for (String name : entities.keySet())
-        {
+        }
+        for (String name : entities.keySet()) {
             OvationEntity e = entities.get(name);
-            if (e != null)
+            if (e != null) {
                 addEntity(name, e);
+            }
         }
     }
-   
-    public void finishEditing()
-     {
-         if (namedEntitiesTable.getCellEditor() != null)
-         {
+
+    public void finishEditing() {
+        if (namedEntitiesTable.getCellEditor() != null) {
             namedEntitiesTable.getCellEditor().stopCellEditing();
-         }
-     }
+        }
+    }
 
     @Override
     public ExplorerManager getExplorerManager() {
@@ -226,5 +259,5 @@ public abstract class NamedEntitySelectionPanel extends JPanel implements
     public IEntityWrapper getSelectedEntity() {
         return entitiesLookup.lookup(IEntityWrapper.class);
     }
-    
+
 }
