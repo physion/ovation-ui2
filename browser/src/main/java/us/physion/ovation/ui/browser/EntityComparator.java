c85a798aa67996b412615954b1ceb80c5d04bb06
Barry Wark <barrywark@gmail.com>
2014-07-02 11:22:15 -0400

    Restoring some sorting sanity

--- browser/src/main/java/us/physion/ovation/ui/browser/EntityComparator.java
+++ browser/src/main/java/us/physion/ovation/ui/browser/EntityComparator.java
@@ -1,26 +1,56 @@
-
 package us.physion.ovation.ui.browser;
 
 import java.util.Comparator;
+import us.physion.ovation.domain.Measurement;
+import us.physion.ovation.domain.OvationEntity;
+import us.physion.ovation.domain.Project;
+import us.physion.ovation.domain.Protocol;
+import us.physion.ovation.domain.Source;
 import us.physion.ovation.domain.mixin.TimelineElement;
 
 /**
  * Comparator for Ovation entities. Compares start time for time line elements,
  * or display name for all others.
- * @param <T>  entity type T
+ *
+ * @param <T> entity type T
  */
-public class EntityComparator<T extends EntityWrapper> implements Comparator<T>{
+public class EntityComparator<T extends EntityWrapper> implements Comparator<T> {
 
     @Override
     public int compare(T o1, T o2) {
-        if(o1 instanceof TimelineElement && o2 instanceof TimelineElement) {
-            TimelineElement t1 = (TimelineElement)o1;
-            TimelineElement t2 = (TimelineElement) o2;
+        final OvationEntity entity1 = o1.getEntity(true);
+        final OvationEntity entity2 = o2.getEntity(true);
+
+        if (entity1 instanceof Project && entity2 instanceof Project) {
+            return ((Project) entity1).getName().toLowerCase().compareTo(((Project) entity2).getName().toLowerCase());
+        }
+
+        if (o1.getEntity() instanceof Source && o2.getEntity() instanceof Source) {
+            return ((Source) entity1).getLabel().toLowerCase().compareTo(((Source) entity2).getLabel().toLowerCase());
+        }
+
+        if (o1.getEntity() instanceof Protocol && o2.getEntity() instanceof Protocol) {
+            return ((Protocol) entity1).getName().toLowerCase().compareTo(((Protocol) entity2).getName().toLowerCase());
+        }
+
+        if (entity1 instanceof TimelineElement && entity2 instanceof TimelineElement) {
+            TimelineElement t1 = (TimelineElement) entity1;
+            TimelineElement t2 = (TimelineElement) entity2;
             return t1.getStart().compareTo(t2.getStart());
         }
-        
-        return o1.getEntity(true).getURI().compareTo(o2.getEntity(true).getURI());
-        //return o1.getDisplayName().toLowerCase().compareTo(o2.getDisplayName().toLowerCase());
+
+        if (entity1 instanceof Measurement && entity2 instanceof Measurement) {
+            Measurement m1 = (Measurement) entity1;
+            Measurement m2 = (Measurement) entity2;
+
+            if (m1.getEpoch().equals(m2.getEpoch())) {
+                return m1.getName().toLowerCase().compareTo(m2.getName().toLowerCase());
+            }
+
+            return ((Measurement) entity1).getEpoch().getStart().compareTo(((Measurement) entity2).getEpoch().getStart());
+        }
+
+        return entity1.getURI().compareTo(entity2.getURI());
     }
-    
+
 }
