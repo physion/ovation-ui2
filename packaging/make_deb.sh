e8684498b2846a02fabf95444ddd6fe0d72dc872
Barry Wark <barrywark@gmail.com>
2014-07-23 15:33:32 -0400

    Updating package build
    Add description and epoch for RPM and deb

--- packaging/make_deb.sh
+++ packaging/make_deb.sh
@@ -22,6 +22,7 @@ cp ../application/src/main/linux/ovation_48x48.png $ICON_DIR/ovation.png
 cp -r ../application/target/ovation/* $TARGET_DIR
 
 # Build the package
-fpm --force -s dir -t deb -n ovation -v $VERSION --license gpl3 --vendor Physion --category Science -d 'couchdb >= 1.4' -d 'java7-runtime' --url http://ovation.io -m 'Barry Wark <support@ovation.io>' $TARGET_DIR=/opt $ICON_DIR=/usr/share/icons/hicolor/48x48 $DESKTOP_INFO_DIR=/usr/share
+DESCRIPTION="Ovation Desktop"
+fpm --force -s dir -t deb -n ovation -v $VERSION --epoch 1 --license gpl3 --vendor Physion --category Science -d 'couchdb >= 1.4' -d 'java7-runtime' --url http://ovation.io -m 'Physion <support@ovation.io>' --description "$DESCRIPTION"  $TARGET_DIR=/opt $ICON_DIR=/usr/share/icons/hicolor/48x48 $DESKTOP_INFO_DIR=/usr/share
 
 lintian ovation_$VERSION*.deb
