62e69ee7ff03d858173aa5e9d87eeab22c990e17
Barry Wark <barry@physion.us>
2015-02-06 01:39:22 -0500

    Add windows_release script

---
+++ windows_release.ps1
@@ -0,0 +1,16 @@
+$release_version = $args[0]
+$tag ="v"+$release_version
+
+write-output "===== Checking out tag ====="
+git checkout $tag
+git clean -fdx
+
+
+write-output "===== Building App ===="
+mvn -DskipTests -projects application --also-make clean install
+write-output "===== Building Installer ===="
+mvn -Pdeployment -Pdeploy-windows --projects application install
+
+write-output $tag
+write-output $release_version
+write-output $dev_version
